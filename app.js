4666d4d8b48f1c04ea4bfe9da3aee506c1b56875
monserratsilva <149416602+monserratsilva@users.noreply.github.com>
2023-11-27 14:42:51 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
+const $n = document.querySelector('.name'); // Corregido el selector para coincidir con la clase 'name'
 const $b = document.querySelector('#blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+async function displayUser(username) {
+  $n.textContent = 'Cargando...'; // Corregido el texto de carga
+  try {
+    const response = await fetch(`${usersEndpoint}/${username}`);
+    const data = await response.json(); // Parsear la respuesta a JSON
+    console.log(data);
+    $n.textContent = data.name; // Eliminado el uso de comillas simples
+    $b.textContent = data.blog;
+    $l.textContent = data.location;
+  } catch (err) {
+    handleError(err); // Llamar a la función de manejo de errores en caso de error
+  }
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err.message}`; // Corregido para mostrar el mensaje de error
 }
 
 displayUser('stolinski').catch(handleError);
